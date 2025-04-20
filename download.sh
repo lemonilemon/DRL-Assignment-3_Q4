798b6b1345f1bb041098611b5d62a65af72a0550
lemonilemon <imlemonilemon@gmail.com>
2025-04-20 22:33:34 +0800

    add .gitignore and download.sh

---
+++ download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -L -o rainbow_mario_model_final.pth "https://www.dropbox.com/scl/fi/d1rz9dh4loytpm70ame83/rainbow_mario_model_final.pth?rlkey=y21qljha6wj24t4iozmsvwx1p&st=aoahc2au&dl=0"
