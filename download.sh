0afeb3bea1e5d6a4f72be88f9fff2a1db8c0efc3
lemonilemon <imlemonilemon@gmail.com>
2025-05-05 23:29:50 +0800

    update the agent

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L -o rainbow_mario_model_final.pth "https://www.dropbox.com/scl/fi/d1rz9dh4loytpm70ame83/rainbow_mario_model_final.pth?rlkey=y21qljha6wj24t4iozmsvwx1p&st=dv10a3lo&dl=0"
+curl -L -o rainbow_mario_model_final.pth "https://www.dropbox.com/scl/fi/d1rz9dh4loytpm70ame83/rainbow_mario_model_final.pth?rlkey=y21qljha6wj24t4iozmsvwx1p&st=ulta8lf4&dl=0"
